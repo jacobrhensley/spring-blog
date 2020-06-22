afccd83c4247868a6d175c50f13ad108f60d4707
Jacob Hensley <jacobrhensley3@gmail.com>
2020-06-22 10:59:01 -0500

    completed post controller

---
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String postIndex() {
+        return "posts index page";
+    }
+
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String onePost() {
+        return "view an individual post";
+    }
+
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String submitPost() {
+        return "create a new post";
+    }
+}
